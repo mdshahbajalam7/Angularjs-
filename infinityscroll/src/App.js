f0c73bae4dec2f642636b674121a6ff420e0a1ab
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2022-10-14 19:23:04 +0530

    not Done

---
+++ infinityscroll/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+const page_Number = 1;
+
+function App() {
+  const [state, setstate] = useState([]);
+  const [page, setpage] = useState(page_Number);
+  useEffect(() => {
+    fetch(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=10`)
+      .then((res) => res.json())
+      .then((json) => setstate([...state, ...json]));
+  }, [page]);
+
+
+  const scrollToEnd = ()=>{
+    setpage(page+1)
+  }
+  Window.onscroll = function () {
+    // /check if the page is scroll top to bottom
+    if (
+      window.innerHeight + document.documentElement.scrollTo ===
+      document.documentElement.offsetHeight
+    ) {
+      scrollToEnd()
+    }
+  };
+  return (
+    <div className="App">
+      {state.length > 0 && state.map((elem,i )=>(
+        <div key={elem.id} className="container">
+          <h4>{elem.id}</h4>
+          <h4></h4>
+          <h4></h4>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
